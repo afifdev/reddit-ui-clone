0de1549dd136605d0c5f036989076d8efc1c89ae
afifdev <afifudin.fdn@gmail.com>
2021-08-19 11:41:48 +0700

    fixing extend style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       fontFamily: {
         'ibm': ['IBM-Plex-Sans', 'sans-serif'],
         'noto': ['Noto-Sans', 'sans-serif']
+      },
+      colors: {
+        'reddit-bg': '#DAE0E6',
+        'reddit-fg': '#FFFFFF',
+        'reddit-acc': '#0079D3',
+        'reddit-orange': '#FF4500'
       }
+    },
+  },
+  variants: {
+    extend: {
     },
   },
   plugins: [],
