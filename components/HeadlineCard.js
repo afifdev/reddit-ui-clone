faa09670623c6ae4e4c8ada8a90bd49e7f000677
afifdev <afifudin.fdn@gmail.com>
2021-08-19 23:53:43 +0700

    HeadlineCard

---
+++ components/HeadlineCard.js
@@ -0,0 +1,31 @@
+export default function HeadlineCard({ title, text, source, image }) {
+  const bgImage = {
+    backgroundImage: `url('/assets/images/${image}')`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    <div
+      className="h-44 rounded-lg before:bg-gradient-to-b before:from-black before:to-white"
+      style={bgImage}
+    >
+      <div className="h-full w-full p-3 bg-gradient-to-b from-transparent to-black rounded-lg flex flex-col justify-end text-white">
+        <p className="font-medium text-lg">{title}</p>
+        <p className="font-noto text-sm line-clamp-2">{text}</p>
+        <div className="flex justify-start items-center pt-2">
+          {source ? (
+            <>
+              <div className="w-4 h-4 rounded-full bg-reddit-orange mr-1"></div>
+              <p className="text-xs">{`r/${source} and more`}</p>
+            </>
+          ) : (
+            <>
+              <div className="w-4 h-4 rounded-full mr-1"></div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
