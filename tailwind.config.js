36323842044a3d55385b6fe14c6a61ae0125be07
afifdev <afifudin.fdn@gmail.com>
2021-08-19 23:53:30 +0700

    Adding line-clamp for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'ibm': ['IBM-Plex-Sans', 'sans-serif'],
-        'noto': ['Noto-Sans', 'sans-serif']
+        ibm: ["IBM-Plex-Sans", "sans-serif"],
+        noto: ["Noto-Sans", "sans-serif"],
       },
       colors: {
-        'reddit-bg': '#DAE0E6',
-        'reddit-fg': '#FFFFFF',
-        'reddit-acc': '#0079D3',
-        'reddit-orange': '#FF4500'
-      }
+        "reddit-bg": "#DAE0E6",
+        "reddit-fg": "#FFFFFF",
+        "reddit-acc": "#0079D3",
+        "reddit-orange": "#FF4500",
+      },
+      screens: {
+        rsm: "508px",
+        rmd: "728px",
+        rlg: "948px",
+        rxl: "960px",
+        r2xl: "1020px",
+      },
     },
   },
   variants: {
-    extend: {
-    },
+    extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
